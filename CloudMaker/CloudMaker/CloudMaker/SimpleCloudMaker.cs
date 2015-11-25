b7ca049c525fd0c19b5870d9b090c492ec446ad1
qpPyKT79 <el_razor@mail.ru>
2015-11-25 11:20:43 +0500

    сделал extension методы из cloudMaker`s методов

--- CloudMaker/CloudMaker/CloudMaker/SimpleCloudMaker.cs
+++ CloudMaker/CloudMaker/CloudMaker/SimpleCloudMaker.cs
@@ -8,58 +8,22 @@ namespace CloudMaker.CloudMaker
 {
     public class SimpleCloudMaker :ICloudMaker
     {
-        private int Width { get; set; }
-        private int Height { get; set; }
         public SimpleCloudMaker()
         {
-            Width = 0;
-            Height = 0;
         }
+        
 
-        public List<CloudTag> CreateCloud(List<string> source, int minSize, int maxSize)
-        {
-            var tags = MakeCloudTags(source);
-            tags = SetSize(tags);
-            tags = Shuffle(tags);
-            return SetLocatons(tags);
-        }
+        public List<CloudTag> CreateCloud(List<string> source, int minSize, int maxSize) => 
+            MakeCloudTags(source).SetSize().Shuffle().SetLocatons();
 
-        private List<CloudTag> SetSize(List<CloudTag> tags)
-        {
-            var newTags = new List<CloudTag>();
-            using (Image tempImage = new Bitmap(1, 1))
-            using (var g = Graphics.FromImage(tempImage))
-                foreach (var tag in tags)
-                    newTags.Add(tag.SetSize(g.MeasureString(tag.Word, new Font("Times New Roman",((float)(Math.Log(tag.Frequency,2)+1)*10)))));
-            return newTags;
-        }
 
-        private static List<CloudTag> Shuffle(List<CloudTag> tags)
-        {
-            Random rnd = new Random();
-            return tags.OrderBy((item) => rnd.Next()).ToList();
-        }
+        
 
-        private List<CloudTag> MakeCloudTags(List<string> source) => source.GroupBy(word => word)
+        private static List<CloudTag> MakeCloudTags(List<string> source) => source.GroupBy(word => word)
             .OrderByDescending(word => word.Count())
             .Select(words => new CloudTag(words.First()).SetFrequency(words.Count())).ToList();
         
-        private List<CloudTag> SetLocatons(List<CloudTag> tags)
-        {
-            foreach (var tag in tags)
-            {
-                Width += (int)tag.TagSize.Width+1;
-                Height += (int)tag.TagSize.Height + 1;
-            }
-            ArevaloRectanglePacker packer = new ArevaloRectanglePacker(Width, Height);
-            for (var i = 0; i < tags.Count; i++)
-            {
-                Microsoft.Xna.Framework.Point placement;
-                packer.TryPack((int)tags[i].TagSize.Width, (int)tags[i].TagSize.Height, out placement);
-                tags[i] = tags[i].SetLocation(placement.X, placement.Y);
-            }
-            return tags;
-        }
+        
         
 
 
---
+++ CloudMaker/CloudMaker/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nuclex.Game.Packing;
+
+namespace CloudMaker
+{
+    public static class Extensions
+    {
+        public static List<CloudTag> SetSize(this List<CloudTag> tags)
+        {
+            var newTags = new List<CloudTag>();
+            using (Image tempImage = new Bitmap(1, 1))
+            using (var g = Graphics.FromImage(tempImage))
+                foreach (var tag in tags)
+                    newTags.Add(
+                        tag.SetSize(g.MeasureString(tag.Word,
+                            new Font("Times New Roman", ((float)(Math.Log(tag.Frequency, 2) + 1) * 10)))));
+            return newTags;
+        }
+
+        public static List<CloudTag> Shuffle(this List<CloudTag> tags)
+        {
+            Random rnd = new Random();
+            return tags.OrderBy(item => rnd.Next()).ToList();
+        }
+
+        public static List<CloudTag> SetLocatons(this List<CloudTag> tags)
+        {
+            var width = 0;
+            var height = 0;
+            foreach (var tag in tags)
+            {
+                width += (int)tag.TagSize.Width + 1;
+                height += (int)tag.TagSize.Height + 1;
+            }
+            ArevaloRectanglePacker packer = new ArevaloRectanglePacker(width, height);
+            for (var i = 0; i < tags.Count; i++)
+            {
+                Microsoft.Xna.Framework.Point placement;
+                packer.TryPack((int)tags[i].TagSize.Width, (int)tags[i].TagSize.Height, out placement);
+                tags[i] = tags[i].SetLocation(placement.X, placement.Y);
+            }
+            return tags;
+        }
+    }
+}
